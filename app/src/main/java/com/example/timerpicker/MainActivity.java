6934cc000f97f73ac8521e1b05d7e92f07a3c1eb
mintck22 <chawanluckkm@gmail.com>
2023-02-11 14:29:28 +0700

    timer picker

---
+++ app/src/main/java/com/example/timerpicker/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.timerpicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button timeButton;
+    int hour, minute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        timeButton = findViewById(R.id.timeButton);
+    }
+
+    public void popTimePicker(View view){
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                hour = selectedHour;
+                minute = selectedMinute;
+                timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d",hour,minute));
+            }
+        };
+
+
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener, hour, minute, true);
+
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
+    }
+}
